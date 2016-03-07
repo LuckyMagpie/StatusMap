adfc111b1e642eca123376c4e8c8bd0ac6ddb17f
LuckyMagpie <diego.sse7@gmail.com>
2016-03-07 14:40:39 -0600

    Indicator detail model

--- Django/Ubi/status_point/models.py
+++ Django/Ubi/status_point/models.py
@@ -6,10 +6,18 @@ class StatusPoint(models.Model):
     address = models.CharField(max_length=255)
     contact = models.CharField(max_length=200)
     phone = models.CharField(max_length=150)
-    indicator = models.IntegerField()
+    indicator_count = models.IntegerField()
     latitude = models.FloatField()
     longitude = models.FloatField()
 
     def __str__(self):
-        return '%s - %s (%s)' %(self.name, self.short_name, self.indicator)
+        return '%s - %s (%s)' %(self.name, self.short_name, self.indicator_count)
+
+class Indicator(models.Model):
+    status_point = models.ForeignKey(StatusPoint, related_name='indicator_detail')
+    url = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '(%s) %s' %(self.status_point.name, self.description)
 
--- Django/Ubi/status_point/serializers.py
+++ Django/Ubi/status_point/serializers.py
@@ -1,7 +1,13 @@
 from .models import *
 from rest_framework import serializers
 
+class IndicatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Indicator
+        exclude = ()
+
 class StatusPointSerializer(serializers.ModelSerializer):
+    indicator_detail = IndicatorSerializer(many=True)
     class Meta:
         model = StatusPoint
         exclude = ()
