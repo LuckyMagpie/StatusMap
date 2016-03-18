1faef2b7ab45f33ba12ff518a631eebb024b103f
LuckyMagpie <diego.sse7@gmail.com>
2016-03-18 12:27:24 -0600

    Extra filter lte_gte

--- Django/Ubi/status_point/views.py
+++ Django/Ubi/status_point/views.py
@@ -19,20 +19,26 @@ def routes(request, format=None):
 OPERATORS = {
         'lte' : lambda count: StatusPoint.objects.filter(indicator_count__lte=count).order_by('-name'),
         'gte' : lambda count: StatusPoint.objects.filter(indicator_count__gte=count).order_by('-name'),
+        'lte_gte' : lambda count, count2 : StatusPoint.objects.filter(indicator_count__lte=count, indicator_count__gte=count2).order_by('-name'),
         'e'   : lambda count: StatusPoint.objects.filter(indicator_count=count).order_by('-name')
 }
 class StatusPointView(APIView):
     def get(self, request, format=None):
         indicator_count = request.GET.get('indicator_count','')
+        indicator_count_2 = request.GET.get('indicator_count_2','')
         operator = request.GET.get('q','')
         overall_status = request.GET.get('overall_status', '')
 
         if indicator_count and operator:
             filter = OPERATORS.get(operator)
             if not filter:
-                return Response({'message':'Incorrect Operator (lte, gte, e)'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'message':'Incorrect Operator (lte, gte, lte_gte, e)'}, status=status.HTTP_400_BAD_REQUEST)
 
-            points = filter(indicator_count)
+            if indicator_count_2:
+                points = filter(indicator_count, indicator_count_2)
+            else:
+                points = filter(indicator_count)
+
             points_json = StatusPointSerializer(points, many=True).data
             return Response(points_json, status=status.HTTP_200_OK)
 
